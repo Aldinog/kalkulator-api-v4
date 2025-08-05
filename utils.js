7433bf91f415c4dec66912e3e8e625bd0a3058e1
Aldinog <satriaaldino96@gmail.com>
2025-08-05 17:07:29 +0700

    Update z utils.js

--- utils.js
+++ utils.js
@@ -21,11 +21,11 @@ function calculatePips(pair, op, harga) {
   audcad: 0.0001,
   audnzd: 0.0001,
   // JPY pairs (2 decimal pip)
-  usdjpy: 0.01, 
-  eurjpy: 0.01,
-  gbpjpy: 0.01,
-  audjpy: 0.01,
-  nzdjpy: 0.01,
+  usdjpy: 0.001, 
+  eurjpy: 0.001,
+  gbpjpy: 0.001,
+  audjpy: 0.001,
+  nzdjpy: 0.001,
   // XAU/USD (gold)
   xauusd: 0.01,
 };
@@ -33,11 +33,11 @@ function calculatePips(pair, op, harga) {
 
 const pip = pipValues[pair.toLowerCase()] || 0.0001;
 let entry = harga;
-let h = op === 'buy' ? harga - 20 * pip : harga + 20 * pip;
-let sl = op === 'buy' ? harga - 35 * pip : harga + 35 * pip;
-let tp1 = op === 'buy' ? harga + 20 * pip : harga - 20 * pip;
-let tp2 = op === 'buy' ? harga + 60 * pip : harga - 60 * pip;
-let tp3 = op === 'buy' ? harga + 100 * pip : harga - 100 * pip;
+let h = op === 'buy' ? harga - 200 * pip : harga + 200 * pip;
+let sl = op === 'buy' ? harga - 350 * pip : harga + 350 * pip;
+let tp1 = op === 'buy' ? harga + 200 * pip : harga - 200 * pip;
+let tp2 = op === 'buy' ? harga + 600 * pip : harga - 600 * pip;
+let tp3 = op === 'buy' ? harga + 100 * pip : harga - 1000 * pip;
 
 let decimalPlaces = pair.toLowerCase() === "xauusd" ? 2 : 3;
 
@@ -77,6 +77,7 @@ async function sendTelegramMessage(msg) {
 }
 
 module.exports = { calculatePips, sendTelegramMessage };
+
 
 
 
