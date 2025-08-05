a1da218f5029ff91c4f1b89bb03c0cf97abe4b9f
Aldinog <satriaaldino96@gmail.com>
2025-08-05 18:37:30 +0700

    Update y utils.js

--- utils.js
+++ utils.js
@@ -21,11 +21,11 @@ function calculatePips(pair, op, harga) {
   audcad: 0.0001,
   audnzd: 0.0001,
   // JPY pairs (2 decimal pip)
-  usdjpy: 0.001, 
-  eurjpy: 0.001,
-  gbpjpy: 0.001,
-  audjpy: 0.001,
-  nzdjpy: 0.001,
+  usdjpy: 0.01, 
+  eurjpy: 0.01,
+  gbpjpy: 0.01,
+  audjpy: 0.01,
+  nzdjpy: 0.01,
   // XAU/USD (gold)
   xauusd: 0.01,
 };
@@ -77,6 +77,7 @@ async function sendTelegramMessage(msg) {
 }
 
 module.exports = { calculatePips, sendTelegramMessage };
+
 
 
 
